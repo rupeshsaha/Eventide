4b11ae920be1dc51fc8d377939480ddcff631e0c
Rupesh Saha <155124334+rupeshsaha@users.noreply.github.com>
2025-01-10 19:10:23 +0530

    add feature export to json

---
+++ src/utils/ExportEvents.js
@@ -0,0 +1,25 @@
+export const exportEvents = (month,year) ={
+    // Retrieve the array from localStorage
+    const arrayData = localStorage.getItem("events");
+    
+    // Convert it to a Blob
+    const blob = new Blob([arrayData], { type: "application/json" });
+
+    // Create a link element
+    const a = document.createElement("a");
+
+    // Set the download URL
+    a.href = URL.createObjectURL(blob);
+
+    // Set the filename
+    a.download = "myArray.json";
+
+    // Append the link to the body (not always necessary)
+    document.body.appendChild(a);
+
+    // Trigger the download
+    a.click();
+
+    // Remove the link (cleanup)
+    document.body.removeChild(a);
+}
